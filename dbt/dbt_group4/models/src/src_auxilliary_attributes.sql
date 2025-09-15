5219a1b0cb4f252a1e83bd8e80c0bb62a667333c
Alexander Hrachovina <alexander.hrachovina@stud.sti.se>
2025-09-15 11:26:53 +0200

    dim_aux done

--- dbt/dbt_group4/models/src/src_auxilliary_attributes.sql
+++ dbt/dbt_group4/models/src/src_auxilliary_attributes.sql
@@ -3,7 +3,7 @@ with stg_job_ads as (select * from {{ source('job_ads', 'stg_ads') }} )
 select
     id,
     experience_required,
-    driving_license_required,
+    driving_license_required as driver_license,
     access_to_own_car
 from 
     stg_job_ads
