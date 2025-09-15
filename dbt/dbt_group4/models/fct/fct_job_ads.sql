eff6fcd9a3df9e40732c3bb44c0f032981664883
Erik Unevik <erikunevik@gmail.com>
2025-09-15 16:10:16 +0200

    fct version 2025-09-15

--- dbt/dbt_group4/models/fct/fct_job_ads.sql
+++ dbt/dbt_group4/models/fct/fct_job_ads.sql
@@ -8,7 +8,10 @@
 
 -- from job_ads
 
-with job_ads as (
+
+-- Debbies
+
+{# with job_ads as (
 
     select *
     from {{ ref('src_job_ads') }}
@@ -20,4 +23,93 @@ select
     vacancies,
     relevance,
     application_deadline
-from job_ads
+from job_ads #}
+
+--- Kuckhans--- 
+
+{# with ja as (select * from {{ ref('src_job_ads') }}),
+
+jd as (select * from {{ ref('src_job_details') }}),
+
+e as (select * from {{ ref('src_employer') }})
+
+select
+    {{ dbt_utils.generate_surrogate_key(['jd.id', 'jd.headline']) }} as job_details_key,
+    {{ dbt_utils.generate_surrogate_key(['jd.id', 'e.employer_name']) }} as employer_key,
+
+    vacancies,
+    relevance,
+    application_deadline,
+
+    e.employer_name,
+    jd.description #}
+
+
+---- test
+{# 
+with ja as (select * from {{ ref('src_job_ads') }}),
+
+o as (select * from {{ ref('src_occupation') }}),
+
+jd as (select * from {{ ref('src_job_details') }}),
+
+e as (select * from {{ ref('src_employer') }})
+
+a as (select * from {{ ref('src_auxilliary_attributes.sql') }})
+
+select
+    {{ dbt_utils.generate_surrogate_key(['o.id', 'o.occupation']) }} occupation_id,
+    {{ dbt_utils.generate_surrogate_key(['jd.id', 'jd.headline']) }} as job_details_id,
+    {{ dbt_utils.generate_surrogate_key(['e.id', 'e.employer_name']) }} as employer_id,
+    {{ dbt_utils.generate_surrogate_key(['a.id', 'a.experience_required']) }} as auxilliary_attributes_id,
+
+
+    vacancies,
+    relevance,
+    application_deadline,
+
+    e.employer_name,
+    jd.description
+
+
+    
+from 
+    ja
+left join
+    ja ON o.id = ja.id
+left join
+    e ON o.id = e.id
+left join
+    a on a.id = e.id #}
+
+
+with
+  ja as (select * from {{ ref('src_job_ads') }}),                 
+  jd as (select * from {{ ref('src_job_details') }}),             
+  e  as (select * from {{ ref('src_employer') }}),                
+  a  as (select * from {{ ref('src_auxilliary_attributes') }}),   
+  o  as (                                                          
+    select distinct occupation, occupation_group, occupation_field
+    from {{ ref('src_occupation') }}
+  )
+
+select
+  -- surrogate keys 
+  {{ dbt_utils.generate_surrogate_key(['o.occupation']) }}         as occupation_id,
+  {{ dbt_utils.generate_surrogate_key(['jd.id', 'jd.headline']) }} as job_details_id,
+  {{ dbt_utils.generate_surrogate_key(['e.employer_name']) }}      as employer_id,
+  {{ dbt_utils.generate_surrogate_key(['a.id']) }}                 as auxilliary_attributes_id,
+
+  -- attribut
+  ja.vacancies,
+  ja.relevance,
+  ja.application_deadline,
+  e.employer_name,
+  jd.headline,
+  jd.description
+
+from ja
+left join jd on jd.id = ja.id
+left join e  on e.id  = ja.id
+left join a  on a.id  = ja.id
+left join o  on o.occupation = ja.occupation__label;
