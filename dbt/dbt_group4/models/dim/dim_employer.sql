b72c73b2185e3601abbcda9c86209223eff58f8f
Alexander Hrachovina <alexander.hrachovina@stud.sti.se>
2025-09-17 10:56:34 +0200

    marts done

--- dbt/dbt_group4/models/dim/dim_employer.sql
+++ dbt/dbt_group4/models/dim/dim_employer.sql
@@ -32,18 +32,15 @@ WITH src_employer as (select * from {{ ref('src_employer') }})
 select 
 
     {{ dbt_utils.generate_surrogate_key(['id', 'employer_name']) }} AS employer_id,
-
- 
-
   employer_name,
   employer_workplace,
   employer_organization_number,
-  workplace_street_address,
+  {{ capitalize_first_letter("coalesce(workplace_street_address, 'Address ej specificerad')") }} AS workplace_street_address,
+  {{ capitalize_first_letter("coalesce(workplace_postcode, 'Postnummer ej specificerad')") }} AS workplace_postcode,
+  {{ capitalize_first_letter("coalesce(workplace_city, 'Stad ej specificerad')") }} AS workplace_city,
   workplace_region,
-  workplace_postcode, 
-  {{ capitalize_first_letter("coalesce(workplace_city, 'stad ej specificerad')") }} AS workplace_city,
   workplace_country,
   
-
- from src_employer
+ from 
+    src_employer
 
